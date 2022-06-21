50c3f72e981fe5bcfc3b61db4694f99ef18bbb94
Siar Wahidi <siar.wahidi@gmail.com>
2022-06-21 11:19:20 -0400

    tested route. Made small change to readme

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const app = express();
 
+// routes
+app.get("/hello", (req, res) => {
+  res.send(`Task Manager App`);
+});
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
